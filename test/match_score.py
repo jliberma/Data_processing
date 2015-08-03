fd1e4729950dee8c6379a2b2898ec3ca8e236bfc
jacobliberman <jacobliberman@gmail.com>
2015-08-03 01:04:31 -0500

    Added combined program, removed partial

--- test/match_score.py
+++ test/match_score.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 
 ###########################################
-#
 # match_report - parse IRB 7s match reports 
-#
 # written by jliberman@utexas.edu
-#
 ###########################################
 
 import csv
 import itertools
-import glob
 import os
 import sys
 
@@ -24,8 +20,8 @@ from pdfminer.layout import LAParams
 
 # convert the PDF to text
 def write_txt(pdf):
-    outfile = pdf.split("_")[4] + "_" + \
-        pdf.split("_")[1] + "_" + pdf.split("_")[0] + ".txt"
+    outfile = pdf.split("_")[4] + "_" + pdf.split("_")[1] + "_" + \
+        pdf.split("_")[0] + ".txt"
     pagenos = set([2,3])
 
     codec = 'utf-8'
@@ -33,7 +29,6 @@ def write_txt(pdf):
     laparams = LAParams()
 
     rsrcmgr = PDFResourceManager(caching=caching)
-    # try with statement here
     outfp = file(outfile, 'w+')
     device = TextConverter(rsrcmgr, outfp, codec=codec, laparams=laparams)
     fp = file(pdf, 'rb')
@@ -42,46 +37,50 @@ def write_txt(pdf):
         interpreter.process_page(page)
     fp.close()
     device.close()
-
-    #with open(outfp, 'r') as txt_file:
-        #lines = txt_file.readlines()
-    outfp.seek(0, 0)
-    lines = outfp.readlines()
-    #outfp.seek(0, 0)
-    outfp.writelines(lines[8:-8])
-        #open('test2.txt', 'w').writelines(lines[8:-8])
-        #datafile.writelines(lines[8:-8])
     outfp.close()
 
-    # return outfile name
-    return
+    # remove header and footer from text file
+    with open(outfile, 'r') as txtin:
+        lines = txtin.read().splitlines(True)
+    with open(outfile, 'w') as txtout:
+        txtout.writelines(lines[8:-8])
 
+    return outfile
 
-# parse text to dictionary lists
-def parse_text():
+# convert text to csv
+def write_csv(txt):
+    
     out = {}
-    with open('d2.txt', 'r') as in_file:
-        stripped = (line.strip() for line in in_file)
-        lines = (line for line in stripped if line)
+    out['match'] = []
+    out['event'] = []
+
+    # create output filename
+    event = txt.split("_")[1]
+    match = txt.split("_")[0]
+    outfile = match + "_" + event + "_" + "scoring" + ".csv"
+
+    # parse text to dictionary lists
+    with open(txt, 'r') as in_file:
+        lines = (line.strip() for line in in_file)
         for line in lines:
 	    if line.startswith(('H', "Description", "Time", "Team")):
 	            header = line.strip()
 		    out[header] = []
 	    else:
 		    out[header].append(line.strip('\n'))
-    return    
 
+    # populate match & event dictionary lists
+    for x in out[header]:
+        out['match'].append(match)
+        out['event'].append(event)
 
-# write lists to a csv file
-def write_csv():
-    event = []
-    with open('d2.csv', 'w') as out_file:
+    # write to the csv
+    with open(outfile, 'w') as out_file:
         writer = csv.writer(out_file)
-        writer.writerow(("Event","Match","Half","Time","Team","Type"))
-        # join event, match, and dictionary lists to row
-        for row in itertools.izip(event, match, out['H'], out['Time'], out['Team'], out['Description']):
-        # write to csv named with event and match number
+	writer.writerow(("Event","Match","Half","Time","Team","Type"))
+        for row in itertools.izip(out['event'], out['match'], out['H'], out['Time'], out['Team'], out['Description']):
             writer.writerows([row])
+    
     return
 
 
@@ -94,7 +93,10 @@ def main(argv):
 	sys.stderr.write("ERROR: %s not found.\n" % (argv[1],))
         return 1
 
-    write_txt(argv[1])
+    txt = write_txt(argv[1])
+    csv = write_csv(txt)
+
+    return
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
