2800d8a791ba0c7363d828f80df8f2ebe60e68b8
jacobliberman <jacobliberman@gmail.com>
2015-08-15 12:57:55 -0500

    renamed directories

---
+++ scripts/get_penalties.py
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+x=0
+y=0
+
+if [ -f full_pen.csv ]
+then
+	rm full_pen.csv
+fi
+
+if [ ! -x match_penalty.py ]
+then
+	echo "ERROR: match_penalty.py missing."
+	exit 1
+fi
+
+function get_penalties {
+	for i in $(ls *.pdf)
+	do
+		((x++))	
+		echo "processing $i"
+		./match_penalty.py $i 2> $i.err && rm $i.err
+	done
+}
+
+function cat_csv {
+	echo "Match,Event,Ref,Half,Time,Team,Description,Reason" > full_pen.csv
+	for i in $(ls *penalty.csv | cut -f2 -d_ | sort -u)
+	do
+		for j in $(ls *_$i*csv | sort -n | grep -v full)
+		do
+       			((y++))	
+			echo "writing $j"
+			cat $j >> full_pen.csv 
+			echo "removing $j"
+			rm $j
+		done
+	done
+}
+
+function report {
+	z=$(echo "scale=3; $y/$x" | bc -l)
+	echo  "$y of $x ($z%) files parsed successfully."
+
+	if [ $x -ne $y ]
+	then
+		echo "The following files did not parse:"
+		ls *.err
+	fi
+}
+
+get_penalties
+cat_csv
+report
+rm *.txt
