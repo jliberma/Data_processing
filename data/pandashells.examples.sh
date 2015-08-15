63c430cf2369b6191d51dd5c2861d00b1f8ae8cd
jacobliberman <jacobliberman@gmail.com>
2015-08-15 17:54:29 -0500

    commented pandashells examples

--- data/pandashells.examples.sh
+++ data/pandashells.examples.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 
+# select rows from match 1 in Wellington
 cat full_pen.csv | p.df 'df[df.Match==1]' 'df[df.Event=="Wellington"]' -o table
+# count penalties by each ref
 cat full_pen.csv | p.df 'df.groupby(by="Ref").Description.count()' -o table header index 
+# only defensive penalties
 cat full_pen.csv | p.df 'df[df.Description.str.contains("defence")]' -o table 
+# defensive penalties from Wellington
 cat full_pen.csv | p.df 'df[df.Event=="Wellington"]' 'df[df.Description.str.contains("defence")]' -o table 
 # plot penalties per event
 # plot penalties by ref
