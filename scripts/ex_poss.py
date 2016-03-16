da2705b9b438f6d65ff9f64ef8e4c696d8a2e66b
jacobliberman <jacobliberman@gmail.com>
2016-03-16 00:10:29 -0400

    remove comments, add libraries

--- scripts/ex_poss.py
+++ scripts/ex_poss.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 
-#import csv
 import pandas as pd
-#import numpy as np
-#import datetime
-
-# save the possession data in a
-# dictionary object
-#csvfile = open('full_poss.csv', 'rb')
-#reader = csv.DictReader(csvfile)
-
-#for row in reader:
-#	print row
+import numpy as np
+import datetime
 
 df = pd.read_csv('full_poss.csv')
+
 df.info()
 print df.describe()
 print df.head()
