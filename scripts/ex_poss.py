41495453a02f648868bb512b00c7c17a8807bceb
jacobliberman <jacobliberman@gmail.com>
2016-03-17 00:08:50 -0400

    convert mmss to seconds in data frame using map function

--- scripts/ex_poss.py
+++ scripts/ex_poss.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import numpy as np
-import datetime
+
+def to_seconds(mmss):
+	seconds= 0
+	for sect in mmss.split(':'):
+		seconds = seconds * 60 + int(sect)
+	return seconds
 
 df = pd.read_csv('full_poss.csv')
 
-df.info()
-print df.describe()
-print df.head()
+# why can't I do this?
+#df[['H1','H2','Total']] = df[['H1','H2','Total']].map(to_seconds)
+
+# convert the time series to seconds
+df['H1'] = df['H1'].map(to_seconds)
+df['H2'] = df['H2'].map(to_seconds)
+df['Total'] = df['Total'].map(to_seconds)
+print df[df.columns[4:7]].head()
