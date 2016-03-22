7bb17a28f1757ae29c783d24dd758e477e4ae1c3
jacobliberman <jacobliberman@gmail.com>
2016-03-21 22:08:35 -0400

    add time of possession statistics

--- scripts/ex_poss.py
+++ scripts/ex_poss.py
@@ -1,44 +1,51 @@
 #!/usr/bin/env python
 
 import pandas as pd
-pd.set_option('display.max_rows', 1000)
 import numpy as np
 import matplotlib.pyplot as plt
-#matplotlib.style.use('ggplot')
+
+# make Tufte-style display
+plt.style.use('ggplot')
 
 def to_seconds(mmss):
-	seconds= 0
-	for sect in mmss.split(':'):
-		seconds = seconds * 60 + int(sect)
-	return seconds
+    seconds= 0
+    for sect in mmss.split(':'):
+        seconds = seconds * 60 + int(sect)
+    return seconds
 
+# TODO: add link to online data source
 df = pd.read_csv('full_poss.csv')
 
-# why can't I do this?
+# TODO: submit to stack exchange for review, why can't I do this?
 #df[['H1','H2','Total']] = df[['H1','H2','Total']].map(to_seconds)
 
 # convert the time series to seconds
 df['H1'] = df['H1'].map(to_seconds)
 df['H2'] = df['H2'].map(to_seconds)
 df['Total'] = df['Total'].map(to_seconds)
-print df.head(8)
-#print len(df.index)
-#df.info()
-#print df[df.columns[4:7]].head()
 
 # split into two data frames
 df['side'] = np.tile([1,2],len(df.index)/2)
-df1 = df[df.side == 1]
-df2 = df[df.side == 2]
+df1 = df[df.side == 1].reset_index(drop=True)
+df2 = df[df.side == 2].reset_index(drop=True)
 
-# do I need to find winner?
-# for correlation,
 # subtract df1 total from df2 total
-# subtract df1 points from df2 points
-# correlate, plot
-print sum(df1['Points'].ge(df2['Points']))
-print sum(df1['Points'].lt(df2['Points']))
-#plt.figure()
-df['Points'].plot()
-#plt.show()
+df3 = df1[['Points','Total']] - df2[['Points','Total']]
+
+# find the slope and intercept of the best fit line
+slope,intercept = np.polyfit(df3['Total'],df3['Points'],1)
+
+# TODO: calculate Pearson correlation coefficient
+
+# create a list of values for the best fit line
+ablineValues = slope * df3[['Total']] + intercept
+
+# plot the best fit line over the values
+plt.scatter(df3['Total'],df3['Points'])
+plt.plot(df3['Total'], ablineValues, 'b')
+plt.title(slope)
+plt.show()
 #plt.savefig("temp.png")
+
+# TODO: calculate frequencies
+# win with total, H1, H2 advantage
