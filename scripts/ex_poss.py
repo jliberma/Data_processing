1312a6b4069ffe0d381eacc6627a5a3de6416257
jacobliberman <jacobliberman@gmail.com>
2016-03-21 23:13:15 -0400

    add Tufte-style dot-dash plot

--- scripts/ex_poss.py
+++ scripts/ex_poss.py
@@ -3,49 +3,59 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
-# make Tufte-style display
+# make Tufte-style display dot-dash graph
+# available from: https://github.com/ahupp/etframes/blob/master/etframes.py
+import etframes
 plt.style.use('ggplot')
 
+# function to convert min:sec to sec
 def to_seconds(mmss):
     seconds= 0
     for sect in mmss.split(':'):
         seconds = seconds * 60 + int(sect)
     return seconds
 
-# TODO: add link to online data source
-df = pd.read_csv('full_poss.csv')
+# read the possession data to a csv
+full_poss = pd.read_csv('https://raw.githubusercontent.com/jliberma/Data_processing/master/data/full_poss.csv')
 
 # TODO: submit to stack exchange for review, why can't I do this?
 #df[['H1','H2','Total']] = df[['H1','H2','Total']].map(to_seconds)
 
-# convert the time series to seconds
-df['H1'] = df['H1'].map(to_seconds)
-df['H2'] = df['H2'].map(to_seconds)
-df['Total'] = df['Total'].map(to_seconds)
+# convert min:sec columns to sec
+full_poss['H1'] = full_poss['H1'].map(to_seconds)
+full_poss['H2'] = full_poss['H2'].map(to_seconds)
+full_poss['Total'] = full_poss['Total'].map(to_seconds)
 
-# split into two data frames
-df['side'] = np.tile([1,2],len(df.index)/2)
-df1 = df[df.side == 1].reset_index(drop=True)
-df2 = df[df.side == 2].reset_index(drop=True)
+# split match results into two data frames
+full_poss['side'] = np.tile([1,2],len(full_poss.index)/2)
+team1 = full_poss[full_poss.side == 1].reset_index(drop=True)
+team2 = full_poss[full_poss.side == 2].reset_index(drop=True)
 
-# subtract df1 total from df2 total
-df3 = df1[['Points','Total']] - df2[['Points','Total']]
+# subtract team 2 points and possession from team 1 points and possession
+poss_diff = team1[['Points','Total']] - team2[['Points','Total']]
+
+# calculate Pearson correlation coefficient and p-value
+pc = stats.pearsonr(poss_diff['Points'], poss_diff['Total'])
+print(pc)
 
 # find the slope and intercept of the best fit line
-slope,intercept = np.polyfit(df3['Total'],df3['Points'],1)
-
-# TODO: calculate Pearson correlation coefficient
+slope,intercept = np.polyfit(poss_diff['Total'],poss_diff['Points'],1)
 
 # create a list of values for the best fit line
-ablineValues = slope * df3[['Total']] + intercept
+ablineValues = slope * poss_diff[['Total']] + intercept
 
 # plot the best fit line over the values
-plt.scatter(df3['Total'],df3['Points'])
-plt.plot(df3['Total'], ablineValues, 'b')
-plt.title(slope)
-plt.show()
-#plt.savefig("temp.png")
+plt.scatter(poss_diff['Total'],poss_diff['Points'],c=".5")
+etframes.add_dot_dash_plot(plt.gca(), ys=poss_diff['Points'], xs=poss_diff['Total'])
+plt.plot(poss_diff['Total'], ablineValues, 'b', c="k")
+plt.ylabel("Point differential")
+plt.xlabel("Possession differential (seconds)")
+plt.title("Scoring by time of possession differential")
+plt.savefig("7s_poss_scoring.png")
 
 # TODO: calculate frequencies
 # win with total, H1, H2 advantage
+
+# TODO: plot again without non-core teams
